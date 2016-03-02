7628940c268534f6936b348b1551472262f68e60
Wisam Reid <wisamreid@gmail.com>
2016-03-02 13:56:45 -0800

    replaced bitalloc with Kai's code

--- PerceptualAudioCodec_Origin/codec/bitalloc.py
+++ PerceptualAudioCodec_Origin/codec/bitalloc.py
@@ -177,6 +177,9 @@ def BitAlloc(bitBudget, maxMantBits, nBands, nLines, SMR):
             if (noise_floor==negative_infinity * ones(nBands)).all():
                 break
 
+            elif max(noise_floor)<-90:
+                break
+
             max_smr_band = argmax(noise_floor)
 
             if allocation[max_smr_band] < maxMantBits and (remaining_bits - nLines[max_smr_band]) >= 0:
