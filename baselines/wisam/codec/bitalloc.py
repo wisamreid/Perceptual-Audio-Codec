0aee05258c6bb8014af740fe398d69d364404d0b
Wisam Reid <wisamreid@gmail.com>
2016-03-06 20:56:20 -0800

    Added report TEX and Wisam's Baseline Code

---
+++ baselines/wisam/codec/bitalloc.py
@@ -0,0 +1,288 @@
+# from inspect import getsourcefile
+# import os.path as path, sys
+# current_dir = path.dirname(path.abspath(getsourcefile(lambda:0)))
+# sys.path.insert(0, current_dir[:current_dir.rfind(path.sep)])
+# from lib.abbreviations import *
+from window import *
+from mdct import *
+from psychoac import *
+# import provided.bitalloc_sol as solution
+
+infinity = float('inf')
+negative_infinity = float('-inf')
+
+# Test_Booleans = [True, True, True, True]
+
+# test_uniform_bitalloc = Test_Booleans[0]
+# test_constant_SNR_bitalloc = Test_Booleans[1]
+# test_constant_MNR_bitalloc = Test_Booleans[2]
+# test_bitalloc = Test_Booleans[3]
+
+# Question 1.b)
+def BitAllocUniform(bitBudget, maxMantBits, nBands, nLines, SMR=None):
+    """
+    Return a hard-coded vector that, in the case of the signal use in HW#4,
+    gives the allocation of mantissa bits in each scale factor band when
+    bits are uniformely distributed for the mantissas.
+    """
+
+    allocation = ones(nBands, dtype=int)
+    bits_per_line = int(bitBudget / float(sum(nLines)))
+
+    allocation = allocation * bits_per_line
+
+    # distribute left over bits
+    remaining_bits = bitBudget - (sum(allocation*nLines))
+
+    if remaining_bits:
+
+        line = 0
+        while remaining_bits > 0:
+
+            remaining_bits -= nLines[line%nBands]
+
+            if(remaining_bits < 0):
+                break
+
+            if (allocation[line%nBands] < maxMantBits):
+                allocation[line%nBands] += 1
+
+            line += 1
+
+    # zero out 1 bit allocations (Mid-Tread)
+    allocation[allocation < 2] = 0
+    # no overflow
+    allocation[allocation > maxMantBits] = maxMantBits
+
+    return allocation
+
+
+def BitAllocConstSNR(bitBudget, maxMantBits, nBands, nLines, peakSPL):
+    """
+    Return a hard-coded vector that, in the case of the signal use in HW#4,
+    gives the allocation of mantissa bits in each scale factor band when
+    bits are distributed for the mantissas to try and keep a constant
+    quantization noise floor (assuming a noise floor 6 dB per bit below
+    the peak SPL line in the scale factor band).
+    """
+
+    allocation = zeros(nBands, dtype=int)
+    remaining_bits = bitBudget
+    noise_floor = peakSPL * ones(nBands)
+    dB_per_bit = 6.0
+
+    while remaining_bits > 0:
+
+        max_smr_band = noise_floor.argmax()
+
+        if allocation[max_smr_band] < maxMantBits and (remaining_bits - nLines[max_smr_band]) >= 0:
+
+            allocation[max_smr_band] += 1
+            remaining_bits -= nLines[max_smr_band]
+
+        noise_floor[max_smr_band] -= dB_per_bit
+
+    # zero out 1 bit allocations (Mid-Tread)
+    allocation[allocation < 2] = 0
+    # no overflow
+    allocation[allocation > maxMantBits] = maxMantBits
+
+    return allocation
+
+
+def BitAllocConstMNR(bitBudget, maxMantBits, nBands, nLines, SMR):
+    """
+    Return a hard-coded vector that, in the case of the signal use in HW#4,
+    gives the allocation of mantissa bits in each scale factor band when
+    bits are distributed for the mantissas to try and keep the quantization
+    noise floor a constant distance below (or above, if bit starved) the
+    masked threshold curve (assuming a quantization noise floor 6 dB per
+    bit below the peak SPL line in the scale factor band).
+    """
+
+    allocation = zeros_like(nLines, dtype=int)
+    remaining_bits = bitBudget
+    dB_per_bit = 6.0
+    noise_floor = SMR.copy()
+
+    while remaining_bits > 0:
+
+        max_smr_band = argmax(noise_floor)
+
+        if allocation[max_smr_band] < maxMantBits and (remaining_bits - nLines[max_smr_band]) >= 0:
+
+            allocation[max_smr_band] += 1
+            remaining_bits -= nLines[max_smr_band]
+
+        noise_floor[max_smr_band] -= dB_per_bit
+
+
+    # zero out 1 bit allocations (Mid-Tread)
+    allocation[allocation < 2] = 0
+    # no overflow
+    allocation[allocation > maxMantBits] = maxMantBits
+
+    return allocation
+
+
+# Question 1.c)
+def BitAlloc(bitBudget, maxMantBits, nBands, nLines, SMR):
+    """
+    Allocates bits to scale factor bands so as to flatten the NMR across the spectrum
+
+       Arguments:
+           bitBudget is total number of mantissa bits to allocate
+           maxMantBits is max mantissa bits that can be allocated per line
+           nBands is total number of scale factor bands
+           nLines[nBands] is number of lines in each scale factor band
+           SMR[nBands] is signal-to-mask ratio in each scale factor band
+
+        Return:
+            bits[nBands] is number of bits allocated to each scale factor band
+
+        Logic:
+           Maximizing SMR over block gives optimization result that:
+               R(i) = P/N + (1 bit/ 6 dB) * (SMR[i] - avgSMR)
+           where P is the pool of bits for mantissas and N is number of bands
+           This result needs to be adjusted if any R(i) goes below 2 (in which
+           case we set R(i)=0) or if any R(i) goes above maxMantBits (in
+           which case we set R(i)=maxMantBits).  (Note: 1 Mantissa bit is
+           equivalent to 0 mantissa bits when you are using a midtread quantizer.)
+           We will not bother to worry about slight variations in bit budget due
+           rounding of the above equation to integer values of R(i).
+    """
+
+    allocation = zeros(nBands, dtype=int)
+    noise_floor = SMR.copy()
+    dB_per_bit = 6.0
+    remaining_bits = bitBudget
+
+    optimal = False
+
+    if optimal: # TODO: Fix this
+
+        allocation[allocation >= 2] = array(remaining_bits / sum(nLines[allocation >= 2]) + (1.0 / dB_per_bit) * (SMR[allocation >= 2] - sum(nLines[allocation >= 2] * SMR[allocation >= 2]) / sum(nLines[allocation >= 2])), dtype=int)
+
+        # zero out 1 bit allocations (Mid-Tread)
+        allocation[allocation < 2] = 0
+        # no overflow
+        allocation[allocation > maxMantBits] = maxMantBits
+
+    else:
+
+        counter = 0
+
+        while remaining_bits > 0:
+
+            if (noise_floor==negative_infinity * ones(nBands)).all():
+                break
+
+            max_smr_band = argmax(noise_floor)
+
+            if allocation[max_smr_band] < maxMantBits and (remaining_bits - nLines[max_smr_band]) >= 0:
+
+                allocation[max_smr_band] += 1
+                remaining_bits -= nLines[max_smr_band]
+
+            elif allocation[max_smr_band] >= maxMantBits:
+
+                noise_floor[max_smr_band] = negative_infinity
+
+            noise_floor[max_smr_band] -= dB_per_bit
+            counter += 1
+            if counter > bitBudget:
+                break
+
+
+    # zero out 1 bit allocations (Mid-Tread)
+    allocation[allocation < 2] = 0
+    # no overflow
+    allocation[allocation > maxMantBits] = maxMantBits
+
+    return allocation
+
+
+
+#-----------------------------------------------------------------------------
+
+#Testing code
+if __name__ == "__main__":
+
+
+    FS = 48000.0
+    N = 1024
+
+    # allocation variables
+    maxMantBits = 16
+    scale = 4
+
+    # construct input signal
+    freqs = (420, 530, 640, 840, 4200, 8400)
+    amps = (0.60, 0.11, 0.10, 0.08, 0.05, 0.03)
+    n = arange(N, dtype=float)
+    x = zeros_like(n)
+
+    for i in range(len(freqs)):
+        x += amps[i] * cos(2 * pi * freqs[i] * n / FS)
+
+    nLines = AssignMDCTLinesFromFreqLimits(N/2, FS)
+    sfb = ScaleFactorBands(nLines)
+    nBands = sfb.nBands
+    nLines = sfb.nLines
+
+    bitBudget = int(128.0/(FS/1000.0) * N/2 - (4+4) * nBands - 4)
+
+    mdct = (1 << scale) * MDCT(SineWindow(x), N/2, N/2)
+    SMR = CalcSMRs(x, mdct, scale, FS, sfb)
+
+
+    if test_uniform_bitalloc:
+
+        print "BitAllocUniform Test Results:"
+        print "Solution:"
+        print solution.BitAllocUniform(bitBudget,maxMantBits,nBands,nLines)
+        print "Generated:"
+        print BitAllocUniform(bitBudget,maxMantBits,nBands,nLines)
+
+
+    if test_constant_SNR_bitalloc:
+
+        mdct_spl = SPL(4.0 * (mdct**2.0))
+        peaks = zeros(sfb.nBands)
+
+        for band in range(nBands):
+
+            max = mdct_spl[sfb.lowerLine[band]]
+
+            for line in range(sfb.lowerLine[band]+1,sfb.upperLine[band]+1):
+
+                SPL = mdct_spl[line]
+
+                if (SPL > max):
+                    max = SPL
+
+            peaks[band] = max
+
+        print "BitAllocConstSNR Test Results:"
+        print "Solution:"
+        print solution.BitAllocConstSNR(bitBudget,maxMantBits,nBands,nLines,peaks).astype(int)
+        print "Generated:"
+        print BitAllocConstSNR(bitBudget,maxMantBits,nBands,nLines,peaks)
+
+
+    if test_constant_MNR_bitalloc:
+
+        print "BitAllocConstMNR Test Results:"
+        print "Solution:"
+        print solution.BitAllocConstMNR(bitBudget,maxMantBits,nBands,nLines,SMR).astype(int)
+        print "Generated:"
+        print BitAllocConstMNR(bitBudget,maxMantBits,nBands,nLines,SMR)
+
+
+    if test_bitalloc:
+
+        print "BitAlloc Test Results:"
+        print "Solution:"
+        print solution.BitAlloc(bitBudget,maxMantBits,nBands,nLines,SMR)
+        print "Generated:"
+        print BitAlloc(bitBudget,maxMantBits,nBands,nLines,SMR)
---
+++ baselines/wisam/codec/window.py
@@ -0,0 +1,204 @@
+"""
+-----------------------------------------------------------------------
+window.py -- Defines functions to window an array of data samples
+
+Author: Wisam Reid
+-----------------------------------------------------------------------
+"""
+
+# from mdct_sol import *
+# from window_sol import *
+import numpy as np
+from numpy import *
+import matplotlib.pyplot as plt
+# import window_ as sol
+# import math
+# from numpy.fft import fft, ifft
+# from abbreviations import *
+from mdct import *
+
+
+# Booleans
+problem1d = False
+problem1e = False
+test_KBD = False
+
+### Problem 1.d ###
+def SineWindow(dataSampleArray):
+    """
+    Returns a copy of the dataSampleArray sine-windowed
+    Sine window is defined following pp. 106-107 of
+    Bosi & Goldberg, "Introduction to Digital Audio..." book
+    """
+
+    N = float(len(dataSampleArray))
+    t = arange(N)
+
+    dataSampleArray *= sin((t + 0.5) * pi / N)
+
+    return dataSampleArray
+
+def HanningWindow(dataSampleArray):
+    """
+    Returns a copy of the dataSampleArray Hanning-windowed
+    Hann window is defined following pp. 106-107 of
+    Bosi & Goldberg, "Introduction to Digital Audio..." book
+    """
+
+    N = float(len(dataSampleArray))
+    t = arange(N)
+
+    dataSampleArray *= 0.5 * (1 - cos(2.0 * (t + 0.5) * pi / N))
+
+    return dataSampleArray
+
+### Problem 1.d - OPTIONAL ###
+def KBDWindow(dataSampleArray,alpha=4.):
+    """
+    Returns a copy of the dataSampleArray KBD-windowed
+    KBD window is defined following pp. 108-109 and pp. 117-118 of
+    Bosi & Goldberg, "Introduction to Digital Audio..." book
+    """
+
+    N = float(len(dataSampleArray))
+    t = arange(int(N/2 + 1))
+    input = dataSampleArray.copy()
+
+    # i0 --> 0th modified bessel function
+    kaiser = i0(alpha * pi * sqrt(1.0 - (4.0 * t / N - 1.0) ** 2)) / np.i0(np.pi * alpha)
+
+    denominator =  sum(kaiser ** 2)
+
+    numerator = cumsum(kaiser[:-1] ** 2)
+    numerator = concatenate((numerator, numerator[::-1]), axis=0)
+
+    window = sqrt(numerator /  denominator)
+    out = input * window
+
+    return out
+
+
+#-----------------------------------------------------------------------------
+
+#Testing code
+if __name__ == "__main__":
+
+    if test_KBD:
+
+        N = 1024
+        n = arange(N)
+
+        # figure and plots
+        plt.plot(n, KBDWindow(ones(N)), 'r', label='KBD Window with $\\alpha=4$')
+        plt.plot(n, sol.KBDWindow(ones(N)), 'b', label='KBD Window Solution')
+
+        # labels
+        plt.xlabel('n (Samples)')
+        plt.xlim(0, N - 1)
+        plt.ylabel('Amplitude')
+        plt.title('Implemented Windows')
+        plt.legend(loc=8)
+
+        # display
+        plt.grid()
+        plt.show()
+
+
+    if problem1d: # windows
+
+        N = 1024
+        n = arange(N)
+
+        # figure and plots
+        figure(figsize=(10, 7))
+        plot(n, SineWindow(ones(N)), 'g', label='Sine Window')
+        plot(n, HanningWindow(ones(N)), 'b', label='Hanning Window')
+        plot(n, KBDWindow(ones(N)), 'r', label='KBD Window with $\\alpha=4$')
+
+        # labels
+        xlabel('n (Samples)')
+        xlim(0, N - 1)
+        ylabel('Amplitude')
+        title('Implemented Windows')
+        legend(loc=8)
+
+        # display
+        grid()
+        show()
+
+    if problem1e: # resolution
+
+        FS = 44100.0
+        N = 1024
+        n = arange(N)
+        freq = 3000.0
+
+        # signal
+        x = cos(2 * pi * freq * n / FS)
+
+        # windowed signals
+        x_Sine = SineWindow(x)
+        x_Hann = HanningWindow(x)
+        x_KBD = KBDWindow(x)
+
+        # KBD window gain factor
+        w_KBD = 1.0 / N * sum(KBDWindow(ones(N)))
+
+
+
+        ###### FFT ######
+
+        # FFT Sine
+        fft_x_Sine = fft(x_Sine)[:N / 2]
+        SPL_fft_x_Sine = 96.0 + 10.0 * log10(8.0 / N ** 2 * abs(fft_x_Sine) ** 2)
+
+        # FFT Hann
+        fft_x_Hann = fft(x_Hann)[:N / 2]
+        SPL_fft_x_Hann = 96.0 + 10.0 * log10(8.0 / 3.0 * 4.0 / N ** 2 * abs(fft_x_Hann) ** 2)
+
+        # FFT KBD
+        fft_x_KBD = fft(x_KBD)[:N / 2]
+        SPL_fft_x_KBD = 96.0 + 10.0 * np.log10(1.0 / w_KBD * 4.0 / N ** 2 * abs(fft_x_KBD) ** 2)
+
+
+
+        ###### MDCT ######
+
+        # MDCT Sine
+        mdct_x_Sine = MDCT(x_Sine, N / 2, N / 2)
+        SPL_mdct_x_Sine = 96.0 + 10.0 * log10(4.0 * mdct_x_Sine ** 2)
+
+        # MDCT Hann
+        mdct_x_Hann = MDCT(x_Hann, N / 2, N / 2)
+        SPL_mdct_x_Hann = 96.0 + 10.0 * log10(4.0 * mdct_x_Hann ** 2)
+
+        # MDCT KBD
+        mdct_x_KBD = MDCT(x_KBD, N / 2, N / 2)
+        SPL_mdct_x_KBD = 96.0 + 10.0 * log10(4.0 / w_KBD * mdct_x_KBD ** 2)
+
+        freq_fft = arange(N / 2) * FS / N
+        freq_mdct = freq_fft + FS / N / 2.0
+
+        figure()
+        plot(freq_fft, SPL_fft_x_Sine, 'r-', freq_fft, SPL_fft_x_Hann, 'g-', freq_fft, SPL_fft_x_KBD, 'b-', freq_mdct, SPL_mdct_x_Sine, 'k-', freq_mdct, SPL_mdct_x_Hann, 'm-', freq_mdct, SPL_mdct_x_KBD, 'c-')
+        ylim(-100, 100)
+        xlim(0, FS / 2)
+
+        title('FFT vs MDCT')
+        xlabel('Freq (Hz)')
+        ylabel('SPL (dB)')
+
+        grid()
+        plt.legend(('FFT: Sine Window','FFT: Hanning Window','FFT: KBD Window', 'MDCT: Sine Window','MDCT: Hanning Window', 'MDCT: KBD Window with $\\alpha=4$'))
+        plt.grid()
+
+        figure()
+        plot(freq_fft, SPL_fft_x_Sine, 'r-', freq_fft, SPL_fft_x_Hann, 'g-', freq_fft, SPL_fft_x_KBD, 'b-', freq_mdct, SPL_mdct_x_Sine, 'k-', freq_mdct, SPL_mdct_x_Hann, 'm-', freq_mdct, SPL_mdct_x_KBD, 'c-')
+        ylim(0, 100)
+        xlim(freq - 1000, freq + 1000)
+
+        xlabel('Freq (Hz)')
+        ylabel('SPL (dB)')
+        title('Resolution Comparison')
+        grid()
+        show()
